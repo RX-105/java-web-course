2caac2c2c8462218bf38acfef3af0d9210839112
欸嘿欸嘿 <1436648676@qq.com>
2022-05-18 00:01:52 +0800

    minor changes

--- xhr-and-ajax/index/index.js
+++ xhr-and-ajax/index/index.js
@@ -1,7 +1,24 @@
 "use strict";
 
-// 元素的click()应当只设置一次。
+// XMLHttpRequest请求，修改页面内容
+var xhr = new XMLHttpRequest();
+if (window.XMLHttpRequest) {
+    xhr = new XMLHttpRequest();
+} else {
+    xhr = new ActiveXObject('Microsoft.XMLHTTP');
+}
+xhr.open('GET', 'http://127.0.0.1:8081/api/v1/classroom/getAll');
+xhr.send();
+xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        document.getElementById('mydiv').innerHTML = xhr.responseText;
+    }
+}
 
+// JQuery.load()，然后修改页面内容
+$(".mydiv").load("http://127.0.0.1:8081/api/v1/classroom/getAll", null);
+
+// 元素的click()应当只设置一次。
 // 解析JSON。请求的返回内容是一个JSON字符串。
 // $(".btn").click(() => {
 //     $.ajax({
@@ -29,30 +46,23 @@
 
 
 // JQuery.post()
-$(".btn").click(() => {
-    $.post(
-        'http://127.0.0.1:8081/api/v1/user/login',
-        {
-            username: 'zgy',
-            password: '12345678'
-        },
-        (data,status) => {
-            alert('Data: \n'+JSON.stringify(data)+'\nStatus: '+status);
-        }
-    );
-});
+// $(".btn").click(() => {
+//     $.post(
+//         'http://127.0.0.1:8081/api/v1/user/login',
+//         {
+//             username: 'zgy',
+//             password: '12345678'
+//         },
+//         (data, status) => {
+//             alert('Data: \n' + JSON.stringify(data) + '\nStatus: ' + status);
+//         }
+//     );
+// });
 
-// XMLHttpRequest请求，修改页面内容
-var xhr = new XMLHttpRequest();
-if(window.XMLHttpRequest){
-    xhr = new XMLHttpRequest();
-} else {
-    xhr = new ActiveXObject('Microsoft.XMLHTTP');
-}
-xhr.open('GET','http://127.0.0.1:8081/api/v1/classroom/getAll');
-xhr.send();
-xhr.onreadystatechange = function(){
-    if(xhr.readyState == 4 && xhr.status == 200){
-        document.getElementById('mydiv').innerHTML = xhr.responseText;
-    }
-}
+// 改用axios
+$(".btn").click(() => {
+    axios.post('http://127.0.0.1:8081/api/v1/user/login?username=zgy&password=12345678')
+        .then(resp => {
+            alert('Response: \n' + JSON.stringify(resp['data']));
+        });
+});
